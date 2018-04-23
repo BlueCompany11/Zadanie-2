33e15fc6de23aff5075d5be36a1846d14c6066fb
BlueCompany <gronczewskim@gmail.com>
2018-04-23 21:06:37 +0200

    Added simple reading from xlsx file and button to open FileDialog

--- Zadanie 2/Form1.Designer.cs
+++ Zadanie 2/Form1.Designer.cs
@@ -28,20 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonGetFile = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxXlsxOutput = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // buttonGetFile
+            // 
+            this.buttonGetFile.Location = new System.Drawing.Point(13, 13);
+            this.buttonGetFile.Name = "buttonGetFile";
+            this.buttonGetFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetFile.TabIndex = 0;
+            this.buttonGetFile.Text = "Wybierz plik";
+            this.buttonGetFile.UseVisualStyleBackColor = true;
+            this.buttonGetFile.Click += new System.EventHandler(this.buttonGetFile_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "Wybierz plik";
+            // 
+            // textBoxXlsxOutput
+            // 
+            this.textBoxXlsxOutput.Location = new System.Drawing.Point(13, 43);
+            this.textBoxXlsxOutput.Multiline = true;
+            this.textBoxXlsxOutput.Name = "textBoxXlsxOutput";
+            this.textBoxXlsxOutput.Size = new System.Drawing.Size(396, 314);
+            this.textBoxXlsxOutput.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(421, 369);
+            this.Controls.Add(this.textBoxXlsxOutput);
+            this.Controls.Add(this.buttonGetFile);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonGetFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox textBoxXlsxOutput;
     }
 }
 
--- Zadanie 2/Form1.cs
+++ Zadanie 2/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Zadanie_2
 {
@@ -15,6 +18,63 @@ namespace Zadanie_2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonGetFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog.FileName;
+                try
+                {
+                    Excel.Application xlApp = new Excel.Application();
+                    Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(file);
+                    Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                    Excel.Range xlRange = xlWorksheet.UsedRange;
+
+                    int rowCount = xlRange.Rows.Count;
+                    int colCount = xlRange.Columns.Count;
+                    Console.WriteLine(rowCount.ToString()+" "+colCount.ToString());
+                    for (int i = 1; i <= rowCount; i++)
+                    {
+                        for (int j = 1; j <= colCount; j++)
+                        {
+                            //new line
+                            if (j == 1)
+                                Console.Write("\r\n");
+
+                            //write the value to the console
+                            if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                                Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
+                        }
+                    }
+
+                    //cleanup
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+
+                    //rule of thumb for releasing com objects:
+                    //  never use two dots, all COM objects must be referenced and released individually
+                    //  ex: [somthing].[something].[something] is bad
+
+                    //release com objects to fully kill excel process from running in the background
+                    Marshal.ReleaseComObject(xlRange);
+                    Marshal.ReleaseComObject(xlWorksheet);
+
+                    //close and release
+                    xlWorkbook.Close();
+                    Marshal.ReleaseComObject(xlWorkbook);
+
+                    //quit and release
+                    xlApp.Quit();
+                    Marshal.ReleaseComObject(xlApp);
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Nie udało się otworzyć pliku: " + openFileDialog.FileName);
+                }
+            }
         }
     }
 }
